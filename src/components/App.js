67465327e365d0296b20fd89c7b34b7cfda11d50
Hack Reactor Student <student@hackreactor.com>
2020-01-02 21:14:15 -0800

    Implement App container component

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,42 @@
 import React from 'react';
-import VideoList from './VideoList.js';
-import VideoPlayer from './VideoPlayer.js';
-import Search from './Search.js';
+import VideoListContainer from './VideoList.js';
+import VideoPlayerContainer from './VideoPlayer.js';
+import SearchContainer from './Search.js';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+export default App = () => (
 
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
+  // this.getYouTubeVideos = this.getYouTubeVideos.bind(this);
 
-    this.getYouTubeVideos = this.getYouTubeVideos.bind(this);
-  }
 
-  componentDidMount() {
-    this.getYouTubeVideos('react tutorials');
-  }
+  // getYouTubeVideos(query) {
+  //   var options = {
+  //     key: this.props.API_KEY,
+  //     query: query
+  //   };
 
-  handleVideoListEntryTitleClick(video) {
-    this.setState({currentVideo: video});
-  }
-
-  getYouTubeVideos(query) {
-    var options = {
-      key: this.props.API_KEY,
-      query: query
-    };
-
-    this.props.searchYouTube(options, (videos) =>
-      this.setState({
-        videos: videos,
-        currentVideo: videos[0]
-      })
-    );
-  }
+  //   this.props.searchYouTube(options, (videos) =>
+  //     this.setState({
+  //       videos: videos,
+  //       currentVideo: videos[0]
+  //     })
+  //   );
+  // }
 
   //TODO: swap out the React components below for the container components
   //  you wrote in the 'containers' directory.
-  render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <div className="col-md-6 col-md-offset-3">
-            <Search getYouTubeVideos={this.getYouTubeVideos}/>
-          </div>
-        </nav>
-        <div className="row">
-          <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
-          </div>
-          <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
-          </div>
-        </div>
+  <div>
+    <nav className="navbar">
+      <div className="col-md-6 col-md-offset-3">
+        <SearchContainer />
       </div>
-    );
-  }
-}
-
-export default App;
+    </nav>
+    <div className="row">
+      <div className="col-md-7">
+        <VideoPlayerContainer />
+      </div>
+      <div className="col-md-5">
+        <VideoListContainer />
+      </div>
+    </div>
+  </div>
+);
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
+import exampleVideoData from '../data/exampleVideoData.js'
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 
-const configureStore = (initialState) => {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk)
-  );
+const configureStore = () => {
+  rootReducer,
+  {
+    currentVideo: exampleVideoData[0],
+    videoList: exampleVideoData
+  };
 };
 
 var store = configureStore();
